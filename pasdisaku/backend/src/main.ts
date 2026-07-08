6ab78f7d633bec8b5df4e4b20585639b3d0d9691
borneomks79-svg <borneomks79@gmail.com>
2026-07-08 14:52:52 +0800

    Update main.ts

--- pasdisaku/backend/src/main.ts
+++ pasdisaku/backend/src/main.ts
@@ -6,9 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-    credentials: true,
-  });
+  origin: true,
+  credentials: true,
+});
 
   app.useGlobalPipes(
     new ValidationPipe({
