e78196f803f2cc05962106614c9dbf7f759e0e12
borneomks79-svg <borneomks79@gmail.com>
2026-07-08 12:17:34 +0800

    Update suppliers.module.ts

--- pasdisaku/backend/src/suppliers/suppliers.module.ts
+++ pasdisaku/backend/src/suppliers/suppliers.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bullmq';
 import { SuppliersService } from './suppliers.service';
 import { SuppliersController } from './suppliers.controller';
-import { IMPORT_QUEUE, SYNC_QUEUE } from '../queue/queue.module';
+import { IMPORT_QUEUE, SYNC_QUEUE } from '../queue/queue.constants';
 
 @Module({
   imports: [BullModule.registerQueue({ name: IMPORT_QUEUE }, { name: SYNC_QUEUE })],
