d878c0c91e586960d2511fda78e4c3c5d10fb6e8
borneomks79-svg <borneomks79@gmail.com>
2026-07-09 19:50:53 +0800

    Update orders.module.ts

--- pasdisaku/backend/src/orders/orders.module.ts
+++ pasdisaku/backend/src/orders/orders.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
+import { PublicOrdersController } from './public-orders.controller';
 
 @Module({
-  controllers: [OrdersController],
+  controllers: [OrdersController, PublicOrdersController],
   providers: [OrdersService],
   exports: [OrdersService],
 })
